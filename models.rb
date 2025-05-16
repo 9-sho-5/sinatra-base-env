4f536ee41c0e8d1a78fffbbe2af416f1bdb7f88e
9-sho-5 <hosho.kusu@icloud.com>
2025-05-17 01:20:29 +0900

    fix: models.rb

--- models.rb
+++ models.rb
@@ -1,8 +1,16 @@
 require 'bundler/setup'
 Bundler.require
+require 'yaml'
 
-ActiveRecord::Base.establish_connection
+# Sinatra/Rackの環境（RACK_ENV）がなければ development を使う
+env = ENV['RACK_ENV'] || 'development'
 
-# タイムゾーンの設定
+# DATABASE_URL 優先。なければ config/database.yml を使う。
+db_config = ENV['DATABASE_URL'] || YAML.load_file('config/database.yml')[env]
+
+# ActiveRecord に接続設定を渡す
+ActiveRecord::Base.establish_connection(db_config)
+
+# タイムゾーン設定
 Time.zone = 'Tokyo'
 ActiveRecord::Base.default_timezone = :local
